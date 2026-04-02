c53c2937017e637ad9447005d62d1e036ba5fc5f
Leonardo Armas <leo.armas.rs@gmail.com>
2026-04-02 11:57:54 -0600

    set up frontend and basic routing

---
+++ react-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Hola mundo! xd</h1>
+      <Routes>
+        <Route path='/users' element={<Users/>}/>
+        <Route path='/places/new' element={<NewPlace/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
