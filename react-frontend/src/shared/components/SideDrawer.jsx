abea9cb696d0138ab1d64188506e67ed314788fc
Leonardo Armas <leo.armas.rs@gmail.com>
2026-04-05 12:06:50 -0600

    created a portal for side drawer

--- react-frontend/src/shared/components/SideDrawer.jsx
+++ react-frontend/src/shared/components/SideDrawer.jsx
@@ -1,11 +1,13 @@
+// import ReactDOM from 'react-dom'
+import {createPortal} from 'react-dom'
 import './SideDrawer.css'
 
 function SideDrawer(props) {
     const {children} = props
+    const content = <aside className='side-drawer'>{children}</aside>
     return (
-        <aside className='side-drawer'>
-            {children}
-        </aside>
+        // ReactDOM.createPortal(content,document.getElementById("portal"))
+        createPortal(content,document.getElementById("portal"))
     )
 }
 
