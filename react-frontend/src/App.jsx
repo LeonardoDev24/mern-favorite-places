eb1ee23c2d5f88847e2124a5eef412bd0c7f1708
Leonardo Armas <leo.armas.rs@gmail.com>
2026-04-12 18:52:10 -0600

    rendered user places

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import MainNavigation from './shared/components/MainNavigation'
 import Users from './user/pages/Users'
+import UserPlaces from './places/pages/UserPlaces'
 import NewPlace from './places/pages/NewPlace'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Users/>}/>
           <Route path='/users' element={<Users/>}/>
+          <Route path='/:userId/places' element={<UserPlaces/>}/>
           <Route path='/places/new' element={<NewPlace/>}/>
         </Routes>
       </main>
---
+++ react-frontend/src/places/pages/UserPlaces.jsx
@@ -0,0 +1,34 @@
+import PlaceList from "../components/PlaceList"
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Atop_the_Rock_%288721964134%29.jpg/1280px-Atop_the_Rock_%288721964134%29.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Atop_the_Rock_%288721964134%29.jpg/1280px-Atop_the_Rock_%288721964134%29.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
+        },
+        creator: 'u2'
+    }
+]
+
+function UserPlaces() {
+    return <PlaceList items={DUMMY_PLACES}/>
+}
+
+export default UserPlaces
