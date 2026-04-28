cb7ddfbe51a8fb9f857ccd9aa466ca37137c346b
Leonardo Armas <leo.armas.rs@gmail.com>
2026-04-27 20:08:02 -0600

    rendered nav links conditionally based on context changing

--- react-frontend/src/shared/components/NavLinks.jsx
+++ react-frontend/src/shared/components/NavLinks.jsx
@@ -1,21 +1,25 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { AuthContext } from '../context/auth-context'
 import './NavLinks.css'
 
 function NavLinks(props) {
+    const auth = useContext(AuthContext)
+    const {isLoggedIn} = auth
     return (
         <ul className='nav-links'>
             <li>
                 <NavLink to='/' exact="true">ALL USERS</NavLink>
             </li>
-            <li>
+            {isLoggedIn && <li>
                 <NavLink to='/u1/places'>MY PLACES</NavLink>
-            </li>
-            <li>
+            </li>}
+            {isLoggedIn && <li>
                 <NavLink to='/places/new'>NEW PLACE</NavLink>
-            </li>
-            <li>
+            </li>}
+            {!isLoggedIn && <li>
                 <NavLink to='/auth'>LOG IN</NavLink>
-            </li>
+            </li>}
         </ul>
     )
 }
