dcd2eb69671dd80aaa889d4473ca358503e1868c
Leonardo Armas <leo.armas.rs@gmail.com>
2026-05-10 19:53:36 -0600

    set up user routes

---
+++ express-backend/controllers/users-controllers.js
@@ -0,0 +1,55 @@
+const HttpError = require('../models/http-error')
+
+const DUMMY_USERS = [
+    {
+        id: 'u1',
+        name: 'Max Schwarz',
+        email: 'max.schwarz@email.com',
+        password: '********'
+    },
+    {
+        id: 'u2',
+        name: 'Clark Kent',
+        email: 'clark.kent@duck.com',
+        password: '**********'
+    }
+]
+
+const getUsers = (req,res,next) => {
+    res.json({users: DUMMY_USERS})
+}
+
+const signup = (req,res,next) => {
+    const { name, email, password } = req.body
+
+    const hasUser = DUMMY_USERS.some(u => u.email === email)
+    if (hasUser) {
+        throw new HttpError('Cannot create a user with an existing email',422)
+    }
+    
+    const createdUser = {
+        id: Math.floor(Math.random()*10000),
+        name,
+        email,
+        password
+    }
+
+    DUMMY_USERS.push(createdUser)
+    res.status(201).json({user: createdUser})
+}
+
+const login = (req,res,next) => {
+    const { email, password } = req.body
+    const identifiedUser = DUMMY_USERS.find(u => u.email === email)
+    if (!identifiedUser || identifiedUser.password !== password) {
+        throw new HttpError('Incorrect user email or password',401)
+    }
+
+    res.json({message: 'Logged in successfully!'})
+}
+
+module.exports = {
+    getUsers,
+    signup,
+    login
+}
