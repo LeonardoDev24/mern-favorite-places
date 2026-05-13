76ff6b4cc601bef9812cab6e50e6b386f48ada76
Leonardo Armas <leo.armas.rs@gmail.com>
2026-05-12 19:46:46 -0600

    created geocoding util function

---
+++ express-backend/util/location.js
@@ -0,0 +1,24 @@
+const HttpError = require('../models/http-error')
+
+async function getCoordsForAddress(address) {
+    const response = await fetch(
+        `https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(address)}&access_token=${process.env.MAPBOX_ACCESS_TOKEN}`
+    )
+
+    if (!response.ok) {
+        const error = new HttpError('Could not find a location for the specified address.', 422)
+        throw error
+    }
+
+    const data = await response.json()
+
+    if (!data.features || data.features.length === 0) {
+        throw new HttpError('Could not find location.',404)
+    }
+
+    const [lng,lat] = data.features[0].geometry.coordinates
+    const coordinates = {lat,lng}
+    console.log(coordinates)
+}
+
+module.exports = getCoordsForAddress
