182d4fd0d65226a052e88f65ef6b2ce0ef96d42f
Leonardo Armas <leo.armas.rs@gmail.com>
2026-05-17 16:09:33 -0600

    loaded places by user id

--- react-frontend/src/places/pages/UserPlaces.jsx
+++ react-frontend/src/places/pages/UserPlaces.jsx
@@ -1,37 +1,39 @@
+import { useEffect,useState } from "react"
 import { useParams } from "react-router-dom"
 import PlaceList from "../components/PlaceList"
+import LoadingSpinner from "../../shared/components/LoadingSpinner"
+import ErrorModal from "../../shared/components/ErrorModal"
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Atop_the_Rock_%288721964134%29.jpg/1280px-Atop_the_Rock_%288721964134%29.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Atop_the_Rock_%288721964134%29.jpg/1280px-Atop_the_Rock_%288721964134%29.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: 'u2'
-    }
-]
+import { useHttpClient } from "../../shared/hooks/http-hook"
 
 function UserPlaces() {
+    const [loadedPlaces,setLoadedPlaces] = useState(null)
+    const {isLoading,error,sendRequest,clearError} = useHttpClient()
     const {userId} = useParams()
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
-    return <PlaceList items={loadedPlaces}/>
+
+    const getUserPlaces = async () => {
+        try {
+            const data = await sendRequest(
+                `http://127.0.0.1:4040/api/places/user/${userId}`
+            )
+            setLoadedPlaces(data.places)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    useEffect(() => {
+        getUserPlaces()
+    },[sendRequest,userId])
+    return (
+        <>
+            <ErrorModal error={error} onClear={clearError}/>
+            {isLoading && 
+            <div className="center">
+                <LoadingSpinner/>
+            </div>}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces}/>}
+        </>
+    )
 }
 
 export default UserPlaces
